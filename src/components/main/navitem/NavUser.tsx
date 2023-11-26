6729870a8d44089387ee56f0732fd93802cc00b0
nkc930604@gmail.com <nkc930604@gmail.com>
2023-11-27 02:05:08 +0900

    Style: "Add User Dropdown"
    "Add User Dropdown Styling"

--- src/components/main/navitem/NavUser.tsx
+++ src/components/main/navitem/NavUser.tsx
@@ -1,10 +1,42 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect, useRef, useState } from "react";
+import styled, { keyframes } from "styled-components";
 import { LuChevronDown } from "react-icons/lu";
+import { TbSettingsFilled } from "react-icons/tb";
+import { TbLogout } from "react-icons/tb";
 
 const NavUser = () => {
+  const [isDropDownOn, setIsDropDonwOn] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const openDropdown = () => {
+    if (isDropDownOn === false) {
+      setIsDropDonwOn(true);
+    }
+  };
+
+  const closeDropdown = () => {
+    setIsDropDonwOn(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        closeDropdown();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
   return (
-    <Base>
+    <Base onClick={openDropdown}>
       <UserProfileImgWrapper>
         <UserProfileImg />
       </UserProfileImgWrapper>
@@ -12,6 +44,22 @@ const NavUser = () => {
       <DropDownBtn>
         <LuChevronDown />
       </DropDownBtn>
+      {isDropDownOn && (
+        <DropdownContainer ref={dropdownRef}>
+          <DropdownItem>
+            <BtnWrapper>
+              <TbSettingsFilled />
+            </BtnWrapper>
+            <span>마이페이지</span>
+          </DropdownItem>
+          <DropdownItem>
+            <BtnWrapper>
+              <TbLogout />
+            </BtnWrapper>
+            <span>로그아웃</span>
+          </DropdownItem>
+        </DropdownContainer>
+      )}
     </Base>
   );
 };
@@ -72,4 +120,64 @@ const Base = styled.div`
   &:hover ${UserProfileImg} {
     background-size: 130%;
   }
+
+  position: relative;
+`;
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const DropdownContainer = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: -100px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  gap: 5px;
+
+  background: #fff;
+
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  border-radius: 15px;
+`;
+
+const DropdownItem = styled.div`
+  display: flex;
+  justify-content: start;
+  align-content: center;
+  gap: 5px;
+  font-size: 14px;
+  margin: 0 5px;
+  padding: 10px;
+  border-radius: 10px;
+  animation: ${fadeIn} 0.3s ease-in-out;
+  &:hover {
+    background-color: #f8f8f8;
+  }
+  &:first-child {
+    margin-top: 5px;
+  }
+  &:last-child {
+    margin-bottom: 5px;
+  }
+  cursor: pointer;
+`;
+
+const BtnWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 18px;
 `;
